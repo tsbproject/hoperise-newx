bc50fde045ba3f2ea9903e078ffc21fb735c2d9e
TSB <122724396+tsbproject@users.noreply.github.com>
2024-10-29 19:25:38 +0100

    [committed]

---
+++ sass/layout/footer.php
@@ -0,0 +1,65 @@
+<footer class="footer">
+        <div class="container justify-content-end">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <div class="footer-charity-text">
+                        <img src="img/hoperise-logo-small.png" alt="Footer logo" class="footer__logo">
+                        <h2>HOPERISE CHARITY WORLDWIDE</h2>
+                        <p class="footer__text">We envision a world where individuals are freed from poverty, living transformed lives,
+                             and fulfilling their God-given potential. Our mission is to follow Jesus where the
+                              need is greatest, responding to crises and partnering with local churches and 
+                              organizations to help people lift themselves out of poverty. </p>
+                        <hr>
+                        <p footer__social-icon><a href="#"><i class="fa fa-facebook footer__social-icon" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter footer__social-icon" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-behance footer__social-icon" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-dribbble footer__social-icon" aria-hidden="true"></i></a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-5">
+                            <!-- <div class="footer-text one">
+                                <h3>RECENT POST</h3>
+                                <ul>
+                                    <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
+                                    <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing </a></li>
+                                    <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
+                                    <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing</a></li>
+                                    <li><a href="#"><i class="material-icons">keyboard_arrow_right</i> Consectetur Adipisicing Elit</a></li>
+                                </ul>
+                            </div> -->
+                        </div>
+                        <div class="col-md-4 col-sm-3">
+                            <div class="footer-text two">
+                                <h3>USEFUL LINKS</h3>
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Causes</a></li>
+                                    <li><a href="#">Event</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-4">
+                            <div class="footer-text">
+                                <h3>CONTACT US</h3>
+                                <ul>
+                                    <li><i class="material-icons footer-text__icon">location_on</i>&nbsp; &nbsp;<span class="footer-text__locate">DHL Estate, Arowofela, <br> Magboro,  Ogun State, Nigeria</span></li>
+                                    <li><i class="material-icons location footer-text__icon">email</i>&nbsp; &nbsp;<span class="footer-text__locate">info@hoperisecharity.org</span></li>
+                                    <li><i class="material-icons footer-text__icon">call</i>&nbsp; &nbsp;<span class="footer-text__locate">+234 807 305 9021</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+       
+        <div class="footer_bottom">
+            <p>Copyright @ 2024 <a href="#">Hoperise Charity Inc.</a> All Rights Reserved </p>
+        </div>
+    </footer>
+</div>
---
+++ sass/layout/header.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoperise Charity Worldwide Inc.</title>
+    <link rel="icon" type="image/x-icon" href="/img/hoperise-favicon.png">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    
+</head>
+<body>
+    <header class="fixed-top">
+        <section class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-8 col-xs-12">
+                        <div class="contact d-md-block d-none">
+                            <p><i class="material-icons icon-phone">phone</i><a> <span><a href="#"> +234 (807) 305 9021</a></span> &nbsp; 
+                                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <i class="material-icons icon-email">email</i> <span><a href="#"> info@hoperisecharity.org</a></span></p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="join-us">
+                            <p><a href="volunteers.html">JOIN US NOW</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+       <section class="navigator">
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container-fluid">
+              
+                <div class="navbar-brand" href="index.html"><img class="navbar-brand__logo" src="img/hoperise-logo-small.png" alt="Hoprise logo">
+                <h1 class="navbar-brand__title">Hoperise Charity Worldwide</h1>
+            </div>
+              
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                  <h5 class="offcanvas-title text-light" id="offcanvasNavbarLabel">Hoperise Charity Worldwide</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+
+                    <ul id="nav-list" class="navbar-nav">
+                        <li class="navbar-nav__item ">
+                           <a class="navbar-nav__link" aria-current="page" href="index.html">Home</a>
+                        </li>
+                        
+                      
+                        <li class="navbar-nav__item">
+                          <a class="navbar-nav__link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                           Who we are
+                          </a>
+                        
+                         <ul id="who" class="dropdown-menu dropdown-who">
+                            <li><a class="dropdown-item" href="about-us.html">About us</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="policy.html">Policy</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="rules&reglation.html">Rules & Regulation</a></li>
+                          
+                          </ul>
+                        </li>
+                   
+                            
+              
+              
+                   
+                       
+                        
+                        
+                        <li class="navbar-nav__item">
+                            <a class="navbar-nav__link" href="portfolio.html">Portfolio</a></li>   
+              
+              
+                        <li class="navbar-nav__item">
+                            <a class="navbar-nav__link" href="volunteers.html">Get involved</a></li>      
+                         
+                        
+                        <li class="navbar-nav__item">
+                             <a class="navbar-nav__link" href="donate.html">Donate</a></li>
+
+                        <li class="navbar-nav__item">
+                                <a class="navbar-nav__link" href="Prayerworks.php">Prayerworks</a></li>   
+              
+                       
+                        <li class="navbar-nav__item">
+                        <a class="navbar-nav__link"  href="contact-us.html">Contact us</a></li>    
+                            
+                            
+                      </ul>
+                          
+                </div>
+              </div>
+            </div>
+          </nav>
+        </section>     
+
+    </header>
